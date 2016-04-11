712291c76fbaa1b7f75069d338057deffaf183b2
xianyuxiaoqiang <xianyuxiaoqiang@126.com>
2016-04-11 13:34:01 +0800

    Update cmp.bat

--- C_SimpleCompiler/tools/cmp.bat
+++ C_SimpleCompiler/tools/cmp.bat
@@ -1,2 +1,9 @@
-masm he.asm  he.obj he.lst he.crf
-echo  he | link he.obj
+@echo off
+cd src
+..\C_SimpleCompiler.exe
+..\tools\masm /I..\include MyResult.asm  MyResult.obj MyResult.lst MyResult.crf
+echo  MyResult | ..\tools\link MyResult.obj
+copy MyResult.exe ..\bin
+del *.asm *.CRF *.LST *.OBJ *.tmp *.stx *.exe
+cd ..
+echo on
